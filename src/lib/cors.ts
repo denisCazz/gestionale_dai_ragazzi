2789b5a7ba368eb46aee6c2f6bb0d01b85eeaeff
Denis Cazzulo <deniscazzulo@icloud.com>
2026-09-04 12:15:28 +0200

    aggiornamento nome

--- src/lib/cors.ts
+++ src/lib/cors.ts
@@ -1,7 +1,7 @@
 const DEFAULT_ORIGINS = [
   "http://localhost:4321",
   "http://127.0.0.1:4321",
-  "https://dairagazzi.bitora.it",
+  "https://garavella7.bitora.it",
   "https://www.bardairagazzicarmagnola.it",
   "https://bardairagazzicarmagnola.it",
 ];
